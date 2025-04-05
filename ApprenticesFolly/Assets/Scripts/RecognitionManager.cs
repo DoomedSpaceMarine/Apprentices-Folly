75567719e2beba11a3aa80b1b70e24e196c74f8f
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2025-04-05 11:36:50 +0300

    Store shapeobjects to a list.

--- ApprenticesFolly/Assets/Scripts/RecognitionManager.cs
+++ ApprenticesFolly/Assets/Scripts/RecognitionManager.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class RecognitionManager : MonoBehaviour
 {
     [SerializeField] private GameObject trianglePattern;
+
+    public List<GameObject> shapeObjects;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,13 +19,28 @@ public class RecognitionManager : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
+            SetPatternShape("triangle");
             SetPatternPosition();
+        }
+    }
+
+    private void SetPatternShape(string pattern)
+    {
+        shapeObjects.Clear();
+
+        switch (pattern)
+        {
+            case "triangle":
+                foreach(Transform shape in trianglePattern.GetComponentInChildren<Transform>())
+                {
+                    shapeObjects.Add(shape.gameObject); 
+                }
+                break;
         }
     }
 
     private void SetPatternPosition()
     {
         trianglePattern.transform.position = Input.mousePosition;
-        Debug.Log("Code runs");
     }
 }
