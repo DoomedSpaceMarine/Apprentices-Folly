9997f2b9fc1f1ff018c5f124481ebef25c58190d
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2025-04-05 18:57:18 +0300

    switch wizard book pages

---
+++ ApprenticesFolly/Assets/Scripts/WizardBook.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WizardBook : MonoBehaviour
+{
+    [SerializeField] private GameObject[] pagesLeft;
+    [SerializeField] private GameObject[] pagesRight;
+
+    private int maxPages = 3;
+    private int pageState;
+
+    private void Update()
+    {
+        if(pageState == 0)
+        {
+            pagesLeft[0].SetActive(true);
+            pagesRight[0].SetActive(true);
+            pagesLeft[1].SetActive(false);
+            pagesRight[1].SetActive(false);
+            pagesLeft[2].SetActive(false);
+            pagesRight[2].SetActive(false);
+            pagesLeft[3].SetActive(false);
+            pagesRight[3].SetActive(false);
+        }
+
+        if (pageState == 1)
+        {
+            pagesLeft[0].SetActive(false);
+            pagesRight[0].SetActive(false);
+            pagesLeft[1].SetActive(true);
+            pagesRight[1].SetActive(true);
+            pagesLeft[2].SetActive(false);
+            pagesRight[2].SetActive(false);
+            pagesLeft[3].SetActive(false);
+            pagesRight[3].SetActive(false);
+        }
+
+        if (pageState == 2)
+        {
+            pagesLeft[0].SetActive(false);
+            pagesRight[0].SetActive(false);
+            pagesLeft[1].SetActive(false);
+            pagesRight[1].SetActive(false);
+            pagesLeft[2].SetActive(true);
+            pagesRight[2].SetActive(true);
+            pagesLeft[3].SetActive(false);
+            pagesRight[3].SetActive(false);
+        }
+
+        if (pageState == 3)
+        {
+            pagesLeft[0].SetActive(false);
+            pagesRight[0].SetActive(false);
+            pagesLeft[1].SetActive(false);
+            pagesRight[1].SetActive(false);
+            pagesLeft[2].SetActive(false);
+            pagesRight[2].SetActive(false);
+            pagesLeft[3].SetActive(true);
+            pagesRight[3].SetActive(true);
+        }
+    }
+
+    public void PagesForward(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (pageState < maxPages)
+            {
+                pageState++;
+            }
+        }
+        
+    }
+    public void PagesBackward(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (pageState > 0)
+            {
+                pageState--;
+            }
+        }
+        
+    }
+}
