376877ae2a54feadb14696217404e25403206465
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2025-04-05 12:45:52 +0300

    Event Manager

---
+++ ApprenticesFolly/Assets/Scripts/EventManager.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    //Count Score Event
+    public event Action onCountScore;
+    public void CountScore() => onCountScore?.Invoke();
+
+    //Add Score Event
+    public event Action<int> onAddScore;
+    public void AddScore(int addition) => onAddScore?.Invoke(addition);
+}
