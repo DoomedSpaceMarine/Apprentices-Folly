c8d6ca855e1832d8d8f9f0005238a37892232294
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2025-04-05 16:14:24 +0300

    Player Controller

---
+++ ApprenticesFolly/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+
+    [SerializeField] private TrailRendererManager trailRendererManager; 
+
+    private float xRotation;
+
+    public float xSensitivity = 30f;
+    public float ySensitivity = 30f;
+
+    private Vector2 mouse;
+
+    private void LateUpdate()
+    {
+        if (!trailRendererManager.trailRenderer.emitting) 
+        {
+            //calculate camera rotation for looking up and down
+            xRotation -= (mouse.y * Time.deltaTime) * xSensitivity;
+            xRotation = Mathf.Clamp(xRotation, -70f, 70f);
+            //apply xRotation to camera transform
+            playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+            //rotate player to look left and right
+            transform.Rotate(Vector3.up * (mouse.x * Time.deltaTime) * xSensitivity);
+        }
+        
+    }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        mouse = context.ReadValue<Vector2>();
+    }
+}
---
+++ ApprenticesFolly/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float walkForwardSpeed;
+    [SerializeField] private float walkBackwardSpeed;
+    private Vector3 movementDirection;
+    private Vector3 playerVelocity;
+    private bool speedCooldown;
+
+    [Header("Smooth Movement")]
+    [SerializeField] private float smoothInputSpeed = 0.2f;
+    private float currentSmooth;
+    private Vector2 currentInputVector;
+    private Vector2 smoothInputVelocity;
+
+    [Header("References")]
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        //Reference to character controller component
+        characterController = GetComponent<CharacterController>();
+
+        //Store initial input smoothSpeed. 
+        currentSmooth = smoothInputSpeed;
+    }
+
+    private void Update()
+    {
+        //Processing movement
+        ProcessMovement();
+
+        //Set movement speeds 
+        SetMovementSpeeds();
+    }
+
+    private void ProcessMovement()
+    {
+        currentInputVector = Vector2.SmoothDamp(currentInputVector, movementDirection, ref smoothInputVelocity, smoothInputSpeed);
+        Vector3 move = new Vector3(currentInputVector.x, 0, currentInputVector.y);
+        characterController.Move(transform.TransformDirection(move) * Time.deltaTime * movementSpeed);
+    }
+
+    private void SetMovementSpeeds()
+    {
+        //Walk backwards
+        if (Input.GetKey(KeyCode.S))
+        {
+            movementSpeed = walkBackwardSpeed;
+        }
+        //Fixes problem where speed change after stopping walking backwards is too sudden. Makes the change smooth
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            speedCooldown = true;
+            StartCoroutine(SpeedCooldown());
+        }
+        //Set walk forward speed
+        else if (!Input.GetKey(KeyCode.S) && !speedCooldown)
+        {
+            movementSpeed = walkForwardSpeed;
+        }
+    }
+
+    private IEnumerator SpeedCooldown()
+    {
+        yield return new WaitForSeconds(smoothInputSpeed);
+        speedCooldown = false;
+    }
+
+    //Read movement inputs from PlayerInputs input action
+    public void MoveInput(InputAction.CallbackContext context)
+    {
+        movementDirection = context.ReadValue<Vector2>();
+    }
+}
